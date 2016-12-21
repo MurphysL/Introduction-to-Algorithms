4e36629fefdc72f9e6249ddf4178a4a7aa07cbe2
MurphysL <1619673065@qq.com>
2016-12-21 11:01:58 +0800

    HeapSort

--- Sort.cpp
+++ Sort.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <stdlib.h>
 #define Cutoff 100
+#define MAX 1000
 using namespace std;
+
+typedef struct Heap{
+    int *num;
+    int Size;
+    int Capacity;
+}Heap;
+
+void Swap(int *a , int left , int right);
+void printArray(int *a , int len);
+void PrintHeap(Heap *heap);
 
 /**
  * 简单排序
@@ -18,8 +30,10 @@ using namespace std;
 
 /**
  * 冒泡排序
+ *
  * best T=O(N)
  * worst T=O(N^2)
+ * 适用于链表
  */
 void Bubble_Sort(int *a , int len){
     for(int i = 0 ;i <len ;i ++){
@@ -35,11 +49,30 @@ void Bubble_Sort(int *a , int len){
 }
 
 /**
+ * 选择排序
+ */
+void Selection_Sort(int *a , int len){
+    int i , j;
+    for(i = 0;i < len -1;i ++){
+        int min = i;
+        for(j = i+1; j <len ;j ++){
+            if(a[j] < a[min])
+                min = j;
+        }
+        if(min != i)
+            Swap(a , i , min);
+    }
+}
+
+/**
  * 插入排序
+ *
  * best T=O(N)
  * worst T=O(N^2)
- *
- * T(N,I)=O(N+I)
+ * S=O(1)
+ * 适用于基本有序
+ * 适用于链表
+ * 适用于N较小
  */
 void Insertion_Sort(int *a , int len){
     int  j;
@@ -53,7 +86,41 @@ void Insertion_Sort(int *a , int len){
 }
 
 /**
- *希尔排序(特殊插入排序）
+ * 折半插入排序(哨兵)
+ *
+ * T=O(N^2)
+ * S=O(1)
+ * 不适用于链表
+ * 适用于N较大
+ */
+void printArray2(int *a , int len){
+    for(int i = 1 ;i <= len ;i ++){
+        cout << a[i] <<" ";
+    }
+    cout <<""<<endl;
+}
+void Bin_Insertion_Sort(int *a , int len){
+    for(int i = 2 ;i <=len;i ++){
+        a[0] = a[i];
+        int low = 1 , high = i-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(a[mid] > a[0])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        for(int j = i-1 ;j >=high ;j --)
+            a[j+1] = a[j];
+        a[high+1] = a[0];
+    }
+}
+
+/**
+ * 希尔排序(特殊插入排序)
+ *
+ * 不适用于链表
+ * 适用于N较大,无序
  */
 void Sell_Sort(int *a , int len){
     int Sedgewick[] = {929, 505, 209, 109, 41, 19, 5, 1, 0};//增量序列
@@ -72,9 +139,76 @@ void Sell_Sort(int *a , int len){
 }
 
 /**
+ * 堆排序
+ *
+ * T=O(NLOG2N)
+ * S=O(1)
+ * 不适用于链表
+ * 适用于数据较多时
+ */
+bool isEmpty(Heap *heap){
+    return heap->Size == 0;
+}
+bool isFull(Heap *heap){
+    return heap->Size == heap->Capacity;
+}
+void InitHeap(Heap **heap ,int len , int *a){
+    *heap = new Heap;
+    (*heap)->num = (int *)malloc(11*sizeof(int));
+    (*heap)->Capacity = 100;
+    (*heap)->Size = len;
+    (*heap)->num[0] = MAX;
+    for(int i = 1 ;i <= len ;i ++)
+        (*heap)->num[i] = a[i-1];
+}
+void PercDown(Heap *heap , int node){
+    int X = heap->num[node];
+    int Parent , Child;
+    for(Parent = node ;Parent*2 <= heap->Size ;Parent = Child){
+        Child = Parent * 2;
+        if(Child + 1 <= heap->Size && heap->num[Child] > heap->num[Child +1])
+            Child ++;
+        if(X > heap->num[Child])
+            heap->num[Parent] = heap->num[Child];
+        else
+            break;
+    }
+    heap->num[Parent] = X;
+}
+int DeleteNode(Heap *heap){
+    int node = heap->num[1];
+    heap->num[1] = heap->num[heap->Size];
+    heap->num[heap->Size] = node;
+    heap->Size --;
+    PercDown(heap , 1);
+    return node;
+}
+void PrintHeap(Heap *heap){
+    for(int i = 1 ;i <=heap->Size;i ++)
+        cout << heap->num[i]<< " ";
+    cout <<""<<endl;
+}
+void Heap_Sort(int *a , int len){
+    Heap *heap;
+    InitHeap(&heap , len , a);
+    //建初堆
+    for(int i = heap->Size/2 ;i > 0 ;i--)
+        PercDown(heap , i);
+    PrintHeap(heap);
+    int t[10];
+    for(int j = 0 ;j < 10 ;j ++){
+        a[j] = DeleteNode(heap);
+    }
+}
+
+/**
  * 快速排序
- * best pivot中分 T=O(NlogN)
- * worst pivot[0] T=O(N^2)
+ *
+ * S=O(LOG2N)
+ * best pivot中分 T=O(NlogN) S=O(NLOG2N)
+ * worst pivot[0] T=O(N^2) S=O(N)
+ * 不适用于链表
+ * 适用于N较大,无序
  */
 void Swap(int *a , int left , int right){
     int temp = a[left];
@@ -132,10 +266,21 @@ int main(){
     //Insertion_Sort(a , 10);
     //printArray(a , 10);
 
-    Sell_Sort(a , 10);
-    printArray(a , 10);
+    //int a1[] = {100 , 5 , 2 , 8 , 1 , 9 , 0 ,4 , 3 , 7 , 6};
+    //Bin_Insertion_Sort(a1 , 10);
+    //printArray2(a1 , 10);
+
+    //Sell_Sort(a , 10);
+    //printArray(a , 10);
 
     //QSort(a , 10);
     //printArray(a , 10);
+
+    //Selection_Sort(a , 10);
+    //printArray(a , 10);
+
+    Heap_Sort(a , 10);
+    printArray(a , 10);
+
     return 0;
 }
