cb0863c74076e2f662939dec4174aaed680b0816
MurphysL <1619673065@qq.com>
2016-12-25 12:02:38 +0800

    non-recursive InOderTraverse

--- BinTree.cpp
+++ BinTree.cpp
@@ -8,9 +8,9 @@ typedef struct BinTree{
     BinTree *right;
 }BinTree;
 
-typedef struct Stack{
-    int num;
-    int top;
+typedef struct NodeStack{
+    BinTree **node;
+    int size;
 }Stack;
 
 void InitTree(BinTree **head){
@@ -24,7 +24,6 @@ void InitTree(BinTree **head){
     }else{
         *head = NULL;
     }
-
 }
 
 int Depth(BinTree *bintree){
@@ -65,7 +64,6 @@ void InOrderTraversal(BinTree *bintree){
     InOrderTraversal(bintree->right);
 }
 
-
 void PostOrderTraversal(BinTree *bintree){
     if(bintree != NULL){
         PostOrderTraversal(bintree->left);
@@ -74,17 +72,55 @@ void PostOrderTraversal(BinTree *bintree){
     }
 }
 
+void InitStack(NodeStack **nstack , int num){
+    *nstack = new NodeStack;
+    *(*nstack)->node = (BinTree *)malloc(num * sizeof(BinTree));
+    (*nstack)->size = -1;
+}
+
+bool isEmpty(NodeStack *nstack){
+    return nstack->size == -1;
+}
+
+bool PushStack(NodeStack *nstack , BinTree *bintree){
+    nstack->node[++ nstack->size] = bintree;
+}
+
+BinTree *PopStack(NodeStack *nstack){
+    BinTree *bintree = nstack->node[nstack->size];
+    nstack->size --;
+    return bintree;
+}
+
+void InOrderTraversal2(BinTree *bintree , NodeStack *nstack){
+    BinTree *t = bintree;
+    while(t || !isEmpty(nstack)){
+        if(t){
+            PushStack(nstack , t);
+            t = t->left;
+        }else{
+            BinTree *bt = PopStack(nstack);
+            cout << bt->num <<endl;
+            t = bt->right;
+        }
+    }
+}
 
 int main(){
     BinTree *bintree;
     InitTree(&bintree);
+    NodeStack *nstack;
+    InitStack(&nstack , NodeNum(bintree));
 
-    cout<<"先序遍历"<<endl;
-    PreOrderTraversal(bintree);
-    cout<<"中序遍历"<<endl;
-    InOrderTraversal(bintree);
-    cout<<"后序遍历"<<endl;
-    PostOrderTraversal(bintree);
+//    cout<<"先序遍历"<<endl;
+//    PreOrderTraversal(bintree);
+//    cout<<"中序遍历"<<endl;
+//    InOrderTraversal(bintree);
+//    cout<<"后序遍历"<<endl;
+//    PostOrderTraversal(bintree);
+    cout<<"非递归中序遍历"<<endl;
+    InOrderTraversal2(bintree , nstack);
+
 
     cout <<"树的深度:" << Depth(bintree) <<endl;
     cout <<"结点个数:" <<NodeNum(bintree) <<endl;
