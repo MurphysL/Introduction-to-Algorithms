6754f8cc141aaaff32678b1fe0d034d448c04024
MurphysL <1619673065@qq.com>
2018-09-07 20:57:32 +0800

    :bug: Fix solution of 10.1-2

--- src/C10/code/s10.1-2.kt
+++ src/C10/code/s10.1-2.kt
@@ -1,4 +1,5 @@
 package C10.code
+
 /**
  * 10.1-2
  *
@@ -11,42 +12,46 @@ enum class Position{
 
 class Stack(val capacity : Int = 10){
     val values = IntArray(capacity + 2)
-    var left_top = 0
-    var right_top = capacity + 1
 
-    fun empty() : Boolean = left_top == 0 && right_top == capacity + 1
+    init {
+        values[0] = 0
+        values[capacity + 1] = capacity + 1
+    }
 
-    fun full() : Boolean = left_top + 1 == right_top
+    fun emptyLeft() : Boolean = values[0] == 0
+
+    fun emptyRight() : Boolean = values[capacity + 1] == capacity + 1
+
+    fun full() : Boolean = values[0] + 1 == values[capacity + 1]
 
     fun push(pos : Position, value : Int){
         if(full()){
             throw IndexOutOfBoundsException("overflow")
         }else{
             if(pos == Position.LEFT)
-                values[++ left_top] = value
+                values[++ values[0]] = value
             else
-                values[-- right_top] = value
+                values[-- values[capacity + 1]] = value
         }
     }
 
     fun pop(pos : Position) : Int{
-        if(empty()){
-            throw IndexOutOfBoundsException("underflow")
+        return if(pos == Position.LEFT){
+            if(emptyLeft())
+                throw IndexOutOfBoundsException("underflow")
+            else
+                values[values[0] --]
         }else{
-            if(pos == Position.LEFT){
-                left_top --
-                return values[left_top + 1]
-            }else{
-                right_top ++
-                return values[right_top - 1]
-            }
+            if(emptyRight())
+                throw IndexOutOfBoundsException("underflow")
+            else
+                values[values[capacity + 1] ++]
         }
     }
 }
 
 fun main(args: Array<String>) {
     val stack = Stack(5)
-    //println(stack.C10.code.pop(C10.code.Position.LEFT))
     stack.push(Position.LEFT, 1)
     stack.push(Position.RIGHT, 2)
     stack.push(Position.RIGHT, 3)
@@ -55,6 +60,10 @@ fun main(args: Array<String>) {
     stack.push(Position.LEFT, 4)
     stack.push(Position.LEFT, 5)
     stack.push(Position.LEFT, 6)
-    stack.push(Position.RIGHT, 7)
+    print(stack.full())
+    stack.pop(Position.LEFT)
+    stack.pop(Position.LEFT)
+    stack.pop(Position.LEFT)
+    stack.pop(Position.LEFT)
 }
 
